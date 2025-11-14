6a50afcdbec2709c6bef29c390247594f821efd7
sysy971 <sylvain.seremes@gmail.com>
2025-11-14 17:49:44 +0100

    Ajout de la logique du morpion dans script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+let tour = "X";  
+let grille = ["", "", "", "", "", "", "", "", ""];
+let fin = false;
+
+const combinaisonsGagnantes = [
+    [0,1,2], [3,4,5], [6,7,8], 
+    [0,3,6], [1,4,7], [2,5,8], 
+    [0,4,8], [2,4,6]
+];
+
+function jouer(index) {
+    if (grille[index] !== "" || fin) return;
+
+    grille[index] = tour;
+    document.getElementsByClassName("case")[index].innerText = tour;
+
+    if (verifierGagnant()) {
+        document.getElementById("message").innerText = tour + " a gagné !";
+        fin = true;
+        return;
+    }
+
+    if (!grille.includes("")) {
+        document.getElementById("message").innerText = "Match nul !";
+        fin = true;
+        return;
+    }
+
+    tour = tour === "X" ? "O" : "X";
+}
+
+function verifierGagnant() {
+    return combinaisonsGagnantes.some(c =>
+        grille[c[0]] === tour &&
+        grille[c[1]] === tour &&
+        grille[c[2]] === tour
+    );
+}
+
+function reset() {
+    tour = "X";
+    grille = ["", "", "", "", "", "", "", "", ""];
+    fin = false;
+    document.getElementById("message").innerText = "";
+    const cases = document.getElementsByClassName("case");
+    for (let c of cases) c.innerText = "";
+}
